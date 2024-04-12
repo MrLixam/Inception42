3e74116f30708e138325d87db54863381669ca81
Liam VINCENT <lixam.vincent@gmail.com>
2024-04-12 11:18:51 +0200

    💀

---
+++ srcs/requirements/mariadb/tools/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS '$SQL_DB';"
+mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PWD';"
+mysql -e "GRANT ALL PRIVILEGES ON '$SQL_DB'.* TO '$SQL_USER' IDENTIFIED BY '$SQL_PWD';"
+mysql -e "ALTER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PWD;'
+mysql -e "FLUSH PRIVILEGES;"
